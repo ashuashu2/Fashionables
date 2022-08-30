0a2e1d0d683d87719a404e9b2092ae7ffa2dd54f
ashutosh birthare <ashubirthare.123@gmail.com>
2022-08-30 12:14:30 +0530

    update

--- src/components/Cart/emptyCart.jsx
+++ src/components/Cart/emptyCart.jsx
@@ -6,7 +6,7 @@ import { Link} from "react-router-dom";
 function EmptyCart() {
     return(
         <div>
-            <div><img  height={400} width={600} src={emptycart} alt="" /></div>
+            <div><img style={{marginTop:5}}  height={400} width={600} src={emptycart} alt="" /></div>
             <Link to="/ProductListing" ><button style={{width: 200}} className="card-button">Explore all collection</button></Link>
             
 
