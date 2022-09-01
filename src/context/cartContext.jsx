70ec43fe4d734401f8ccde2bd5162461f28be2f6
ashutosh birthare <ashubirthare.123@gmail.com>
2022-09-01 11:10:14 +0530

    update local storage in cart and wishlist context

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -4,10 +4,18 @@ import {cartReducer} from "../ultils/cartReducer"
 const CartContext = createContext()
 
 const CartProvider = ({children}) =>{
+    function getDataFromLocalStorage1(){
+        const getData = localStorage.getItem("Cart")
+        return getData ? JSON.parse(getData):[]
+    }
 
     
     
-    const [cartState,cartDispatch] = useReducer(cartReducer,{cart:[],TotlePrice:0,Discount:0 ,OrignalPrice:0})
+    const [cartState,cartDispatch] = useReducer(cartReducer,{cart:(getDataFromLocalStorage1()),TotlePrice:0,Discount:0 ,OrignalPrice:0})
+
+    useEffect(() => {
+        localStorage.setItem("Cart",JSON.stringify(cartState.cart))
+      }, [cartState])
  
     return (
         <CartContext.Provider value={{cartState,cartDispatch}}>
--- src/context/wishlistContext.jsx
+++ src/context/wishlistContext.jsx
@@ -1,10 +1,25 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import {wishlistReducer} from "../ultils/wishlistReducer"
 
 const WishlistContext = createContext()
 
 const WishlistProvider = ({children}) =>{
-    const [WishlistState,WishlistDispatch] = useReducer(wishlistReducer,{Wishlist:[]})
+
+    function getDataFromLocalStorage2(){
+        const getData = localStorage.getItem("WishList")
+        return getData ? JSON.parse(getData):[]
+    }
+
+    const [WishlistState,WishlistDispatch] = useReducer(wishlistReducer,{Wishlist:getDataFromLocalStorage2()})
+
+    useEffect(() => {
+        localStorage.setItem("WishList",JSON.stringify(WishlistState.Wishlist))
+      }, [WishlistState])
+      
+      
+      
+
+
     return (
         <WishlistContext.Provider value={{WishlistState,WishlistDispatch}}>
             {children}
